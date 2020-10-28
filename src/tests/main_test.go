ced9180944ceaa639ddac91dc275586ac05d8951
actoure <acherif.toure@gmail.com>
2020-10-28 06:05:16 +0100

    feat: Adding unit tests for the project
    This is a bad process (Writing code before the test) but it is necessary to do it.
    
    we encountered a problem with the second test.
    The url does not consider the value of the id as parameter but as part of url.

---
+++ src/tests/main_test.go
@@ -0,0 +1,35 @@
+package tests
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	model "models"
+	control "controllers"
+)
+
+// Tests the creating document funciton
+func TestCreateDocument(t *testing.T) {
+	req, _ := http.NewRequest("POST", "localhost:8080/create?id=12&name=name1&desc=description1", nil)
+	rec := httptest.NewRecorder()
+	
+	assert.Equal(t, 0, len(model.Documents), "No document has been added yet")
+	control.CreateDocument(rec, req)
+	assert.Equal(t, 1, len(model.Documents), "document has been added")	
+}
+
+
+// Tests removig document funciton
+func TestRemoveDocument(t *testing.T) {
+	size := len(model.Documents)
+
+	assert.Equal(t, size, len(model.Documents), "size of the list of documents not changed yet")
+
+	req, _ := http.NewRequest("DELETE", "localhost:8080/remove", nil)
+	rec := httptest.NewRecorder()
+	
+	control.RemoveDocument(rec, req)
+
+	assert.Equal(t, 0, len(model.Documents), "The only one object has been removed")
+}
