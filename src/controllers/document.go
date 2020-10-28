1bd233260b28d1952aca67b368776bafdb658fb5
actoure <acherif.toure@gmail.com>
2020-10-28 03:44:43 +0100

    feat: checking existing of document before adding it

--- src/controllers/document.go
+++ src/controllers/document.go
@@ -19,15 +19,23 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 func CreateDocument(w http.ResponseWriter, r *http.Request) {
 	params := r.URL.Query()
 
-	id, converr := strconv.Atoi(params.Get("id"))
+	id, _ := strconv.Atoi(params.Get("id"))
 	name := params.Get("name")
 	desc := params.Get("desc")
 
-	if converr == nil && name != "" && desc != "" {
-		model.Documents[id] = model.Document{ID: id, Name: name, Description: desc}
+	// check if the document is present or a parameter is missing
+	if _, isPresent := model.Documents[id]; !isPresent {
+		if name != "" && desc != "" {
+			model.Documents[id] = model.Document{ID: id, Name: name, Description: desc}
+			fmt.Fprintf(w, "A new document has been added.\n{ID : %d, Name: %s, Description: %s}", id, name, desc)
+		} else {
+			fmt.Fprint(w, "One or more arguments missing (name, desc)")
+		}
+		return
 	}
 
-	fmt.Fprintf(w, "A new document has been added.\n{ID : %d, Name: %s, Description: %s}", id, name, desc)
+	// print a message if the document exists
+	fmt.Fprintf(w, "The document %d allready exists\n", id)
 }
 
 // Removes document from the list
